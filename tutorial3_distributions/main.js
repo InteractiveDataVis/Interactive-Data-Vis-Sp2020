04336fcd30b66813cee84a39383279595e5b7823
efrymire <ellie@two-n.com>
2020-02-12 20:18:57 -0500

    remove constants from an object

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,12 +1,10 @@
 /**
  * CONSTANTS AND GLOBALS
  * */
-const constants = {
-  width: window.innerWidth * 0.7,
-  height: window.innerHeight * 0.7,
-  margin: { top: 20, bottom: 40, left: 40, right: 40 },
-  radius: 3,
-};
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 50, left: 60, right: 40 },
+  radius = 5;
 
 /** these variables allow us to access anything we manipulate in
  * init() but need access to in draw().
