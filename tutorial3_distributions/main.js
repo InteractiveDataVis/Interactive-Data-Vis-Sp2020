d0101f640418b30f9883ddfd7088ed852fde6029
efrymire <ellie@two-n.com>
2020-02-13 21:26:05 -0500

    more accessible demo

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,75 +1,67 @@
-/**
- * CONSTANTS AND GLOBALS
- * */
+/* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 5;
 
-/** these variables allow us to access anything we manipulate in
- * init() but need access to in draw().
- * All these variables are empty before we assign something to them.*/
+// these variables allow us to access anything we manipulate in init() but need access to in draw().
+// All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
 
-/**
- * APPLICATION STATE
- * */
+/* APPLICATION STATE */
 let state = {
   data: [],
-  // + ADD STATE VARIABLE FOR INTERACTION
+  party: "All"
 };
 
-/**
- * LOAD DATA
- * */
-d3.json(YOUR_DATA_PATH, d3.autoType).then(raw_data => {
+/* LOAD DATA */
+d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("raw_data", raw_data);
   state.data = raw_data;
   init();
 });
 
-/**
- * INITIALIZING FUNCTION
- * this will be run *one time* when the data finishes loading in
- * */
+/* INITIALIZING FUNCTION */
+// this will be run *one time* when the data finishes loading in 
 function init() {
   // SCALES
-  // + xScale =
-  // + yScale =
 
   // AXES
-  // + const xAxis =
-  // + const yAxis =
 
   // UI ELEMENT SETUP
-  // add dropdown (HTML selection) for interaction
   // HTML select reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
+
   const selectElement = d3.select("#dropdown").on("change", function() {
     // `this` === the selectElement
     // 'this.value' holds the dropdown value a user just selected
 
-    // + SET STATE VARIABLE WITH SELECTED VALUE
+    state.party = this.value
     console.log("new value is", this.value);
     draw(); // re-draw the graph based on this new selection
   });
 
-  // + ADD DROPDOWN OPTIONS
+  // add in dropdown options from the unique values in the data
+  selectElement
+    .selectAll("option")
+    .data(["All", "D", "R", "I"]) // unique data values-- (hint: to do this programmatically take a look `Sets`)
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d);
 
   // + CREATE SVG ELEMENT
 
   // + CALL AXES
 
-  draw();
+  draw(); // calls the draw function
 }
 
-/**
- * DRAW FUNCTION
- * we call this everytime there is an update to the data/state
- * */
+/* DRAW FUNCTION */
+ // we call this everytime there is an update to the data/state
 function draw() {
+  
   // + FILTER DATA BASED ON STATE
 
   const dot = svg
