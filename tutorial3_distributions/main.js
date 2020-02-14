dcf6765a6e4b1b6a0acb8e3dcf9b6f294fbd3328
efrymire <ellie@two-n.com>
2020-02-13 21:31:42 -0500

    final final changes

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -13,7 +13,7 @@ let yScale;
 /* APPLICATION STATE */
 let state = {
   data: [],
-  party: "All"
+  selection: "All" // + YOUR FILTER SELECTION
 };
 
 /* LOAD DATA */
@@ -27,18 +27,17 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in 
 function init() {
-  // SCALES
+  // + SCALES
 
-  // AXES
+  // + AXES
 
-  // UI ELEMENT SETUP
-  // HTML select reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
+  // + UI ELEMENT SETUP
 
   const selectElement = d3.select("#dropdown").on("change", function() {
     // `this` === the selectElement
     // 'this.value' holds the dropdown value a user just selected
 
-    state.party = this.value
+    state.selection = this.value
     console.log("new value is", this.value);
     draw(); // re-draw the graph based on this new selection
   });
@@ -46,7 +45,7 @@ function init() {
   // add in dropdown options from the unique values in the data
   selectElement
     .selectAll("option")
-    .data(["All", "1", "2", "3"]) // unique data values-- (hint: to do this programmatically take a look `Sets`)
+    .data(["All", "1", "2", "3"]) // + ADD UNIQUE VALUES
     .join("option")
     .attr("value", d => d)
     .text(d => d);
@@ -64,12 +63,12 @@ function draw() {
   
   // + FILTER DATA BASED ON STATE
 
-  const dot = svg
-    .selectAll("circle")
-    .data(filteredData, d => d.name)
-    .join(
-      enter => enter, // + HANDLE ENTER SELECTION
-      update => update, // + HANDLE UPDATE SELECTION
-      exit => exit // + HANDLE EXIT SELECTION
-    );
+  // const dot = svg
+  //   .selectAll("circle")
+  //   .data(filteredData, d => d.name)
+  //   .join(
+  //     enter => enter, // + HANDLE ENTER SELECTION
+  //     update => update, // + HANDLE UPDATE SELECTION
+  //     exit => exit // + HANDLE EXIT SELECTION
+  //   );
 }
