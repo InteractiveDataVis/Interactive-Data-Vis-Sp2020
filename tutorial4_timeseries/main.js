2a0a89a87b97c38c6c3a6324703bb11f733188f4
efrymire <ellie@two-n.com>
2020-02-20 17:28:49 -0500

    updates with tutorial 4 changes

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -1,14 +1,10 @@
-/**
- * CONSTANTS AND GLOBALS
- * */
+/* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 3;
+  default_selection = "Select a Country";
 
-/** these variables allow us to access anything we manipulate in
- * init() but need access to in draw().
- * All these variables are empty before we assign something to them.*/
 let svg;
 let xScale;
 let yScale;
@@ -16,44 +12,56 @@ let yScale;
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All", // + YOUR FILTER SELECTION
+  selectedCountry: default_selection, 
 };
 
 /* LOAD DATA */
-// + SET YOUR DATA PATH
-d3.json(YOUR_DATA_PATH, d3.autoType).then(raw_data => {
-  console.log("raw_data", raw_data);
+d3.csv("../../data/populationOverTime.csv",  d => ({
+  year: new Date(d.Year, 0, 1),
+  country: d.Entity,
+  population: +d.Population,
+})).then(raw_data => {
+  console.log("raw_data length:", raw_data.length);
   state.data = raw_data;
   init();
 });
 
 /* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
 function init() {
-  // + SCALES
 
-  // + AXES
+  // SCALES
+  xScale = d3
+    .scaleTime()
+    .domain(d3.extent(state.data, d => d.year))
+    .range([margin.left, width - margin.right]);
 
-  // + UI ELEMENT SETUP
+  yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(state.data, d => d.population)])
+    .range([height - margin.bottom, margin.top]);
+
+  // AXES
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale).tickFormat(d3.format(".2s"));
 
   const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-    state.selection = this.value; // + UPDATE STATE WITH YOUR SELECTED VALUE
+    state.selectedCountry = this.value;
     console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
+    draw(); 
   });
 
   // add in dropdown options from the unique values in the data
   selectElement
     .selectAll("option")
-    .data(Array.from(new Set (state.data.map(d => d.country))))
+    .data([
+      ...Array.from(new Set(state.data.map(d => d.country))),
+      default_selection,
+    ])
     .join("option")
     .attr("value", d => d)
     .text(d => d)
     
-  // this ensures that the selected value is the same as what we have in state when we initialize the options
-  selectElement.property("value", state.selectedCountry)
+    selectElement.property("value", default_selection);
 
   // create an svg element in our main `d3-container` element
   svg = d3
@@ -90,51 +98,42 @@ function init() {
   draw(); // calls the draw function
 }
 
-/**
- * DRAW FUNCTION
- * we call this everytime there is an update to the data/state
- * */
+/* DRAW FUNCTION */
 function draw() {
   // filter the data for the selectedParty
   let filteredData
   if (state.selectedCountry !== null) {
     filteredData = state.data.filter(d => d.country === state.selectedCountry)
+    console.log("filteredData length:", filteredData.length)
   }
 
   const dot = svg
     .selectAll(".dot")
-    .data(filteredData, d => d.year) // use `d.name` as the `key` to match between HTML and data elements
+    .data(filteredData, d => d.year) 
     .join(
-      enter =>
-        // enter selections -- all data elements that don't have a `.dot` element attached to them yet
-        enter
+      enter => enter
           .append("circle")
-          .attr("class", "dot") // Note: this is important so we can identify it in future updates
+          .attr("class", "dot") 
           .attr("stroke", "lightgrey")
           .attr("opacity", 0.5)
           .attr("fill",  "purple")
           .attr("r", radius) 
-          .attr("cy", height - margin.bottom) // initial value - to be transitioned
+          .attr("cy", height - margin.bottom)
           .attr("cx", d => xScale(d.year))
-          .call(enter =>
-            enter
-              .transition() // initialize transition
-              .duration(1000) // duration 1000ms / 1s
-              .attr("cy", d => yScale(d.population)) // started from the bottom, now we're here
+          .call(enter => enter
+              .transition() 
+              .duration(1000)
+              .attr("cy", d => yScale(d.population)) 
           ),
-      update => update.call(enter =>
-          enter
-            .transition() // initialize transition
-            .duration(1000) // duration 1000ms / 1s
-            .attr("cy", d => yScale(d.population)) // started from the bottom, now we're here
+      update => update.call(update => update
+            .transition() 
+            .duration(1000) 
+            .attr("cy", d => yScale(d.population)) 
         ),
-      exit =>
-        exit.call(exit =>
-          // exit selections -- all the `.dot` element that no longer match to HTML elements
-          exit
+      exit => exit.call(exit => exit
             .transition()
-            .delay(d => d.year)
-            .duration(500)
+            .delay((_, i) => i * 50)
+            .duration(50)
             .attr("cy", height - margin.bottom)
             .remove()
         )
