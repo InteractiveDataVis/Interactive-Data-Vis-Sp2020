4e0d152e37e2a21ec4977c8158c3a2e5312f1ba3
auchers <aucher@two-n.com>
2020-03-03 23:19:09 -0500

    clean up and minimal advanced version

--- tutorial6_hierarchical/demo/main.js
+++ tutorial6_hierarchical/demo/main.js
@@ -4,7 +4,7 @@
 const width = window.innerWidth * 0.9,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
-  tooltipSpecs = { width: 100, height: 50 }
+  tooltipSpecs = { width: 100, height: 50 };
 
 let svg;
 let tooltip;
@@ -30,17 +30,14 @@ d3.json("../../data/flare.json", d3.autotype).then(data => {
  * this will be run *one time* when the data finishes loading in
  * */
 function init() {
+  const container = d3.select("#d3-container").style("position", "relative");
 
-  const container = svg = d3
-    .select("#d3-container")
-    .style("position", "relative")
-
-  tooltip = container 
+  tooltip = container
     .append("div")
     .attr("width", 100)
-    .attr("height", 100) 
+    .attr("height", 100)
     .style("position", "absolute")
-    .style("background-color", "white")
+    .style("background-color", "white");
 
   svg = container
     .append("svg")
@@ -51,7 +48,7 @@ function init() {
   // make hierarchy
   const root = d3
     .hierarchy(state.data) // children accessor
-    .count() // sets the 'value' of each level
+    .sum(d => d.value) // sets the 'value' of each level
     .sort((a, b) => b.value - a.value);
 
   // make treemap layout generator
@@ -89,14 +86,17 @@ function init() {
     .attr("width", d => d.x1 - d.x0)
     .attr("height", d => d.y1 - d.y0)
     .on("mouseover", d => {
-      state.hover = { 
-        'translate': [ // center tooltip in rect
-          d.x0 + (d.x1 - d.x0)/2, 
-          d.y0 + (d.y1 - d.y0)/2 ], 
-        'name': d.data.name, 
-        'value': d.data.value }
+      state.hover = {
+        translate: [
+          // center tooltip in rect
+          d.x0 + (d.x1 - d.x0) / 2,
+          d.y0 + (d.y1 - d.y0) / 2,
+        ],
+        name: d.data.name,
+        value: d.data.value,
+      };
       draw();
-    })
+    });
 
   draw(); // calls the draw function
 }
@@ -106,16 +106,19 @@ function init() {
  * we call this everytime there is an update to the data/state
  * */
 function draw() {
-
-  if (state.hover !== null) {
+  if (state.hover) {
     tooltip
-      .html(`
+      .html(
+        `
         <div>Name: ${state.hover.name}</div>
         <div>Value: ${state.hover.value}</div>
-      `)
+      `
+      )
       .transition()
       .duration(500)
-      .style("transform", `translate(${state.hover.translate[0]}px,${state.hover.translate[1]}px)`)
+      .style(
+        "transform",
+        `translate(${state.hover.translate[0]}px,${state.hover.translate[1]}px)`
+      );
   }
-
 }
