ae4c584f1b90fa343016a95351b263704a3b40c5
efrymire <ellie@two-n.com>
2020-03-03 20:34:08 -0500

    initial tooltip

--- tutorial6_hierarchical/demo/main.js
+++ tutorial6_hierarchical/demo/main.js
@@ -3,15 +3,18 @@
  * */
 const width = window.innerWidth * 0.9,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+  margin = { top: 20, bottom: 50, left: 60, right: 40 },
+  tooltipSpecs = { width: 100, height: 50 }
 
 let svg;
+let tooltip;
 
 /**
  * APPLICATION STATE
  * */
 let state = {
   data: null,
+  hover: null,
 };
 
 /**
@@ -27,8 +30,19 @@ d3.json("../../data/flare.json", d3.autotype).then(data => {
  * this will be run *one time* when the data finishes loading in
  * */
 function init() {
-  svg = d3
+
+  const container = svg = d3
     .select("#d3-container")
+    .style("position", "relative")
+
+  tooltip = container 
+    .append("div")
+    .attr("width", 100)
+    .attr("height", 100) 
+    .style("position", "absolute")
+    .style("background-color", "white")
+
+  svg = container
     .append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -73,7 +87,16 @@ function init() {
     .append("rect")
     .attr("fill-opacity", 0.6)
     .attr("width", d => d.x1 - d.x0)
-    .attr("height", d => d.y1 - d.y0);
+    .attr("height", d => d.y1 - d.y0)
+    .on("mouseover", d => {
+      state.hover = { 
+        'translate': [ // center tooltip in rect
+          d.x0 + (d.x1 - d.x0)/2, 
+          d.y0 + (d.y1 - d.y0)/2 ], 
+        'name': d.data.name, 
+        'value': d.data.value }
+      draw();
+    })
 
   draw(); // calls the draw function
 }
@@ -82,4 +105,17 @@ function init() {
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/state
  * */
-function draw() {}
+function draw() {
+
+  if (state.hover !== null) {
+    tooltip
+      .html(`
+        <div>Name: ${state.hover.name}</div>
+        <div>Value: ${state.hover.value}</div>
+      `)
+      .transition()
+      .duration(500)
+      .style("transform", `translate(${state.hover.translate[0]}px,${state.hover.translate[1]}px)`)
+  }
+
+}
