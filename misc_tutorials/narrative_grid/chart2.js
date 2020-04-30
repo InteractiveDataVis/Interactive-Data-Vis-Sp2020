eeaf53e65396e2485127d95837ab589f3354c292
efrymire <ellie@two-n.com>
2020-04-30 15:22:23 -0400

    additional template

---
+++ misc_tutorials/narrative_grid/chart2.js
@@ -0,0 +1,57 @@
+export function chart2() {
+  /**
+  * CONSTANTS AND GLOBALS
+  * */
+  const margin = { top: 20, bottom: 50, left: 60, right: 40 };
+  let svg;
+
+  /**
+   * APPLICATION STATE
+   * */
+  let state = {
+    data: null
+  };
+
+  /**
+   * LOAD DATA
+   * Using a Promise.all([]), we can load more than one dataset at a time
+   * */
+  Promise.all([
+    d3.csv("./data/data2.csv"), 
+  ]).then(([data]) => {
+    state.data = data;
+    console.log("state: ", state);
+    init();
+  });
+
+  /**
+   * INITIALIZING FUNCTION
+   * this will be run *one time* when the data finishes loading in
+   * */
+  function init() {
+
+    svg = d3
+      .select("#d3-container-2")
+      .append("svg")
+
+    draw(); // calls the draw function
+  }
+
+  /**
+   * DRAW FUNCTION
+   * we call this everytime there is an update to the data/state
+   * */
+  function draw() {
+
+    svg.style('background-color', 'mediumseagreen')
+
+    svg.selectAll('text')
+      .data(state.data)
+      .join('text')
+      .attr('dx', '50%')
+      .attr('dy', '50%')
+      .style('text-anchor', 'middle')
+      .text(d => `hello I am mrs SVG number ${d.data}`)
+
+  }
+}
